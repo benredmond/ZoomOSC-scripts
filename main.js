bbd1cf00328b37e2563e5fe149e9eb3204540301
benredmond <bjr0206@gmail.com>
2021-08-09 16:38:09 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,92 @@
+const {Client, Server} = require('node-osc');
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+//ZoomOSC Server (IN) Config
+const zoomOSCServerIp = "127.0.0.1";
+const zoomOSCPortIn = 1234;
+//ZoomOSC Client (OUT) Config
+const zoomOSCClientIp = "127.0.0.1";
+const zoomOSCPortOut = 9090;
+
+const clientZoom = new Client(zoomOSCClientIp, zoomOSCPortOut);
+
+const muteNonCohosts = () => {
+    let userList = [];
+    const serverZoom = new Server(zoomOSCPortIn, zoomOSCServerIp);
+
+    clientZoom.send('/zoom/update')
+    clientZoom.send('/zoom/list', () => {
+        userList = [];
+    });
+
+    serverZoom.on('message', (msg) => {
+        if (msg[0].split("/")[3] === 'list') {
+            // console.log(msg);
+            const userRole = msg[7];
+            // const handRaised = msg[11];
+
+            // 1 is host
+            // 2 is cohost
+            if (userRole === 2)
+                userList.push(msg[4]);
+
+        } else if (msg[0] === '/zoomosc/galleryOrder') { // Finished listing users
+            if (userList.length === 0) {
+                console.log("Error: No co-hosts found");
+                serverZoom.close()
+                return;
+            }
+            clientZoom.send('/zoom/allExcept/zoomID/mute', ...userList)
+            serverZoom.close()
+        }
+    });
+}
+
+const spotlightHandRaised = () => {
+    let userList = [];
+    const serverZoom = new Server(zoomOSCPortIn, zoomOSCServerIp);
+
+    clientZoom.send('/zoom/update')
+    clientZoom.send('/zoom/list', () => {
+        userList = [];
+    });
+
+    serverZoom.on('message', (msg) => {
+        if (msg[0].split("/")[3] === 'list') {
+            const handRaised = msg[11];
+            if (handRaised === 1)
+                userList.push(msg[4]);
+
+        } else if (msg[0] === '/zoomosc/galleryOrder') { // Finished listing users
+            if (userList.length === 0) {
+                console.log("Error: Noone with hands raised");
+                serverZoom.close()
+                return;
+            }
+            for (const zoomID of userList) {
+                clientZoom.send('/zoom/zoomID/addSpot', zoomID)
+            }
+            serverZoom.close()
+        }
+    });
+}
+
+
+let userInput = '';
+(async () => {
+    while (userInput !== 'q') {
+        userInput = await prompt('----------------------\nm - mute users except for cohost\ns - spotlight users with hands raised\nq - quit\n----------------------\n');
+
+        if (userInput === 'm') {
+            muteNonCohosts();
+        } else if (userInput === 's') {
+            spotlightHandRaised()
+        }
+    }
+    rl.close()
+})()
+
+rl.on('close', () => process.exit(0))
